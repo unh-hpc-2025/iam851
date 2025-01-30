f608378e2f7d247a1e133592571a8883f7d990b0
Kai Germaschewski <kai.germaschewski@gmail.com>
2025-01-30 09:10:12 -0500

    class3s: move factorial() function into separate file

--- class3/build.sh
+++ class3/build.sh
@@ -2,4 +2,5 @@
 
 gcc -Wall -c hello.c
 gcc -Wall -c greeting.c
-gcc hello.o greeting.o -o hello
+gcc -Wall -c factorial.c
+gcc hello.o greeting.o factorial.o -o hello
--- class3/hello.c
+++ class3/hello.c
@@ -3,19 +3,6 @@
 
 #include <stdio.h>
 
-int factorial(int n)
-{
-    int i;
-    int retval = 1;
-
-    for (i = 1; i <= n; i++)
-    {
-        retval *= i;
-    }
-
-    return retval;
-}
-
 int main(int argc, char **argv)
 {
     greeting("everyone");
