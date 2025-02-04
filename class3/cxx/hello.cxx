80c359f2e986eeb9b6171a11c5fb6f208d727310
Kai Germaschewski <kai.germaschewski@gmail.com>
2025-02-03 20:26:48 -0500

    class3s/cxx: start over in C++

---
+++ class3/cxx/hello.cxx
@@ -0,0 +1,27 @@
+
+#include <stdio.h>
+
+void greeting(void)
+{
+    printf("Hi there.\n");
+}
+
+int factorial(int n)
+{
+    int i;
+    int retval = 1;
+
+    for (i = 1; i <= n; i++)
+    {
+        retval *= i;
+    }
+
+    return retval;
+}
+
+int main(int argc, char **argv)
+{
+    greeting();
+
+    printf("10 factorial is %d\n", factorial(10));
+}
